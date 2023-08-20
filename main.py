5f5bf91e239a25e70b262ff48f24ea05a7ae25fe
Drumbler <andy132012@gmail.com>
2023-08-20 15:01:39 +0500

    Update

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, send_from_directory, send_file
-from views.products import render_products
+from views.products import render_products, render_main
 
 app = Flask(__name__)
 
-
+@app.route('/')
+def main_page():
+    return render_main()
 @app.route('/products')
 def products_page():
     return render_products()
--- views/products.py
+++ views/products.py
@@ -4,8 +4,13 @@ from views.template_loader import jinja_env
 
 def render_products():
     products = get_products()
-    template = jinja_env.get_template("/products.j2")
+    template = jinja_env.get_template("/products.html")
     output_text = template.render(products=products)
 
     return output_text
+
+
+def render_main():
+    output = jinja_env.get_template("/main.html").render()
+    return output
 # переименовать products
